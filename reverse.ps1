e54d3cca9851fc18be0ba03d086f7ccb94324008
Ender Loc Phan <weintome@gmail.com>
2017-09-22 14:42:47 +0300

    Add files via upload

---
+++ reverse.ps1
@@ -0,0 +1,11 @@
+﻿$p = Read-Host "Enter the prime number"
+$b = Read-Host "Enter the base number"
+
+$ma = Read-Host "Enter the message a"
+$mb = Read-Host "Enter the message b"
+
+
+$as =0
+$ab =0
+$key_a =0 
+$key_b =0
