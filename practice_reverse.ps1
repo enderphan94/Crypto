9a94487ed1e8fc059cd9cf6e69842bfe562082af
Ender Loc Phan <weintome@gmail.com>
2017-10-01 22:02:10 +0300

    Update practice_reverse.ps1

--- practice_reverse.ps1
+++ practice_reverse.ps1
@@ -1,55 +1,113 @@
-﻿[Int]$pA = Read-Host "`nEnter the prime A"
+$check = $true
+
+# This function will check the input is prime number
+function checkPrime{
+
+    param($num)
+
+    if(($num -le 1) -or ($num % 2 -eq 0 -and $num -gt 2)){
+
+        Write-Host "`nThe number you just enter isn't prime number" -ForegroundColor Red
+        $global:check = $false
+        exit
+    }
+
+    for($i = 3; $i -lt $num/2; $i+=2){
+        if($num % $i -eq 0){
+            Write-Host "`nThe number you just enter isn't prime number" -ForegroundColor Red
+            $global:check = $false
+            exit       
+        }
+    }
+
+}
+
+# This function will check the input is empty
+function checkEmpty{
+    param($num)
+
+    if($num -eq ""){
+        Write-Host "`nInput can't be empty, Please enter a prime number" -ForegroundColor Red
+        $global:check = $false
+        exit
+    }
+}
+
+[Int]$pA = Read-Host "`nEnter the prime A"
+checkEmpty -num $pA
+checkPrime -num $pA
 [Int]$pB = Read-Host "`nEnter the prime B"
+checkEmpty -num $pB
+checkPrime -num $pB
 
-$n = $pA*$pB
-$m = ($pA-1)*($pB-1)
+# If the inputs have passed all the checks,it will execute it in main
+function main{
+    $n = $pA*$pB
+    $m = ($pA-1)*($pB-1)
 
-function caculation{
-    param($m)
-    $take = 0
-    for($i=2;$i -lt $m;$i++){
+    # Find $e with the GCD($m,$e)=1
+    function Get-GCD {
+        [CmdletBinding()]
+            Param (
+                [Int32] $a,
+                [Int32] $b
+            )
+        while ($a -ne $b) {
+            if ($a -gt $b) {
+                $a = $a - $b
+            }
+            else {
+                $b = $b - $a
+            }
+        }
+        return $a;   
+    }
 
-        if(($m%$i) -ne 0){
-            $take =$i
+    for($e = 2; $e -lt $m;$e++){
+        $x = Get-GCD -a $m -b $e
+        if($x -eq 1){
             break
-        }
+        } 
     }
-    return $take
-}
 
-$e = $(caculation($m))
+    # ask for message here
+    [int]$mess = Read-Host "`nEnter the message"
 
-[int]$mess = Read-Host "`nEnter the message"
+    $cipher = [math]::Pow($mess,$e)%$n
 
-$cipher = [math]::Pow($mess,$e)%$n
+    function findD{
 
-
-
-
-
-function findD{
-
-    param($m, $e)
-    $d = 0
-    for($k=0;$k -lt 999999;$k++){
+        param($m, $e)
+        $d = 0
+        for($k=1;$k -lt 999999;$k++){
         
-        $getd = (1+$k*$m)%$e
-        if($getd -eq 0){
-            $d = $k        
-            break 
+            #$getd = (1+$k*$m)%$e
+            $getd = ($k*$e)%$m
+            if($getd -eq 1){
+                $d = $k        
+                break 
+            }
         }
+        return (1+$d*$m)/$e
     }
-    return (1+$d*$m)/$e
+
+    $d = $(findD -m $m -e $e)
+    $decryp = [math]::pow($cipher,$d)%$n
+
+    $re = [pscustomobject]@{
+     EncrytionKey= $e,$n;
+     TheMessage=$mess;
+     TheCipher=$cipher;
+     DecryptionKey= $d,$n;
+     Decrypted= $decryp
+    }
+    $re|fl
+
 }
 
-$d = $(findD -m $m -e $e)
-$decryp = [math]::pow($cipher,$d)%$n + $mess
-
-$re = [pscustomobject]@{
- Publickey= $n,$e;
- Secretkey= $m,$d;
- TheMessage=$mess;
- TheCipher=$cipher;
- Decrypted= $decryp
+if($check -eq $true){
+    main
 }
-$re|fl
+else{
+    exit
+}
